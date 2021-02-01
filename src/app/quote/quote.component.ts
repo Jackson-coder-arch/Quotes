00a037b4ffcb9be300dc82793cd034103a4109ca
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-01 19:16:40 +0300

    form validation

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote [] = [
-    new Quote(1, 'Make your life a masterpiece imagine no limitations on what you can be have or do.', 'Auther:Brian Tracy',new Date(1967,3,14)),
-    new Quote(2, 'The best way to get started is to Quit Talking and begin Doing', 'Auther:Walt Disney',new Date(2007,5,7)),
-    new Quote(3, 'The pessimist sees difficulty in every opportunity. The optimist sees the opportunity in every difficulty.', 'Auther: Winston CHurchill',new Date(1957,1,4)),
-    new Quote(4, 'Do not let yesterday take up too much of today.', 'Auther: Will Rogers', new Date(2000,31,4)),
-    new Quote(5, 'You learn more from failure than from success.Do not let it stop you. Failure builds Character.', 'Auther: Unknow',new Date(1987,4,7)),
+    new Quote(1, 'Make your life a masterpiece imagine no limitations on what you can be have or do.', 'Author:Brian Tracy',new Date(1967,3,14)),
+    new Quote(2, 'The best way to get started is to Quit Talking and begin Doing', 'Author:Walt Disney',new Date(2007,5,7)),
+    new Quote(3, 'The pessimist sees difficulty in every opportunity. The optimist sees the opportunity in every difficulty.', 'Author: Winston CHurchill',new Date(1957,1,4)),
+    new Quote(4, 'Do not let yesterday take up too much of today.', 'Author: Will Rogers', new Date(2000,31,4)),
+    new Quote(5, 'You learn more from failure than from success.Do not let it stop you. Failure builds Character.', 'Author: Unknow',new Date(1987,4,7)),
 
   ];
 
