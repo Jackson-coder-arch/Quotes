b8d6fd86f651796a0283ae319a4845b2504eb66d
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-01 14:29:06 +0300

    addition of quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,9 @@ import { Quote } from './quote';
 })
 export class AppComponent {
 
-  quotes:Quote [] = [
-    {id:1, name:'Make your life a masterpiece; imagine no limitations on what you can be, have or do.'},
-    {id:2, name:'The best way to get started is to Quit Talking and begin Doing'},
-    {id:3, name:'The pessimist sees difficulty in every opportunity. The optimist sees the opportunity in every difficulty.'},
-    {id:4, name:'Do not let yesterday take up too much of taday.'},
-    {id:4, name:'You learn more from failure than from success.Do not let it stop you. Failure builds Character.'},
+ 
 
-  ];
+ constructor(){
 
-  // constructor(){
-  //   this.quotes = ['Make your life a masterpiece; imagine no limitations on what you can be, have or do.']
-  // }
+ }
 }
